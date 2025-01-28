a34ecfa28be35f5b1db70fffe2561802fb2ddba8
Dorian <kellerdorian6@gmail.com>
2025-01-28 21:48:48 +0100

    Correction

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -3,17 +3,17 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
-/**
+/*
  lis les symptoms a partir du fichier pour ensuite les traiter
  */
 public interface ISymptomReader<string> {
-	/**
+	/*
 	Permet la lecture du fichier txt
 	retourne un hash map avec les noms des maladie et leur nombres
 	 */
 	Map<String, Integer> GetSymptoms ();
 
-	/**
+	/*
 	Créer une liste pour permettre de la trier ensuite
 	retourne la liste trié
 	 */
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface ISymptomWriter {
-    /**
+    /*
     Ecris dans le fichier result.out la liste des symptoms trié et le nombres de fois ou ils apparaissent
      */
     void writeSymptoms(Map<String, Integer> occurrences, List<String> clesTriees) throws IOException;
